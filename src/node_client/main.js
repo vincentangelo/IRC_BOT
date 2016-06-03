0307199459a7bc29572de200a83087f7503e0d05
Sawyer McBride <sawyer.mcbride@outlook.com>
2016-06-02 21:22:16 -0400

    added debug

--- src/node_client/main.js
+++ src/node_client/main.js
@@ -8,15 +8,23 @@ let app = express();
 let users = [];
 
 app.post('/join', (req, res) => {
+	
 	let channel = req.query.channel;
 	let nick = req.query.nick;
+	debugger;
 	users.push(new Client (channel, nick ) );
+	console.log("connected " + users[0] + " :  "+users.length);
+	console.log(users[0].nick + users[0].channel+ " channe is < ");
 	res.sendStatus(200);
 });
 
 app.post('/send', (req, res) => {
+	
 	let message = req.query.message;
 	let nick = req.query.nick;
+	if(!nick || !message)
+		res.sendStatus(400);
+	debugger;
 	findUser(nick).writeData(message);
 	res.sendStatus(200);
 });
@@ -35,6 +43,7 @@ app.use(express.static('public'));
 
 function findUser( nick ) {
 	users.forEach(function(elem) {
+		console.log(elem.nick + " nick");
 		if(elem.nick === nick ) {
 			return elem;
 		}
